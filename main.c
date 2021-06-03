abfbcbc2a0cec5b772782cf33df5f267b8596061
Al Wilkinson <alfredw@qti.qualcomm.com>
2021-06-03 15:06:52 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <errno.h>
-
+#define QBIT_DMA 0
 int main (void)
 {
 	int fd;
+	int* virtual_addr;
 	char buf[400] ="1234abcdabcdabc1234dabcdabcdabcd1234";
 
 	fd = open("/dev/virtual_pci", O_RDWR);
@@ -19,6 +20,12 @@ int main (void)
 		perror("write: ");
 		return 1;
 	}
+	
+	if(ioctl(fd, QBIT_DMA, virtual_addr);
+	   printf("virt addr 0x%x \n", virtual_addr);
+	   
+	   
+	
 	close(fd);
 
 	return 0;
